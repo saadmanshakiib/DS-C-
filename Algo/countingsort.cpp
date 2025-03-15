c54a64dce65d865879069503e4c9bea60c6dfbfb
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-03-16 01:22:54 +0600

    Add files via upload

---
+++ Algo/countingsort.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+////////////////////
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) { 
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1); 
+        quickSort(arr, pivot + 1, high); 
+    }
+}
+
+/*void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}*/
+///////
+void countingSort(int arr[], int n) {
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+
+    int count[maxVal + 1] = {0};
+
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= maxVal; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int output[n];
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+/////
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int leftArr[n1], rightArr[n2];
+    
+    for (int i = 0; i < n1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+////
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {4,2,2,8,3,3,1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original Array: ";
+    printArray(arr, n);
+
+    countingSort(arr, n);
+    mergeSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted Array: ";
+    printArray(arr, n);
+
+    return 0;
+}
---
+++ Algo/quicksort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) { 
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+
+        quickSort(arr, low, pivot - 1); 
+        quickSort(arr, pivot + 1, high); 
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Initial Array: ";
+    printArray(arr, n);
+    cout << endl;
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted Array: ";
+    printArray(arr, n);
+
+    return 0;
+}
